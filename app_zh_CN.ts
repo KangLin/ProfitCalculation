adced15dfb8f63f72d8ceedffada5ef109d3ab57
KangLin <kl222@126.com>
2015-05-02 12:01:20 +0800

    增加翻译

--- app_zh_CN.ts
+++ app_zh_CN.ts
@@ -10,60 +10,80 @@
     </message>
     <message>
         <location filename="mainwindow.ui" line="23"/>
-        <source>Fee rate:</source>
-        <translation>手续费费率</translation>
+        <source>Fee rates:</source>
+        <oldsource>Fee rate:</oldsource>
+        <translation>手续费费率：</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="40"/>
+        <source>Margin rates:</source>
+        <translation>保证金比例：</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="57"/>
         <source>Count:</source>
         <translation>数量：</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="65"/>
+        <location filename="mainwindow.ui" line="82"/>
         <source>Profilt Calculation:</source>
-        <translation>赢利计算</translation>
+        <translation>赢利计算：</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="73"/>
-        <location filename="mainwindow.ui" line="230"/>
-        <location filename="mainwindow.ui" line="271"/>
+        <location filename="mainwindow.ui" line="90"/>
+        <location filename="mainwindow.ui" line="308"/>
+        <location filename="mainwindow.ui" line="363"/>
         <source>Purchase price:</source>
         <translation>买入价格：</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="83"/>
-        <location filename="mainwindow.ui" line="111"/>
-        <location filename="mainwindow.ui" line="143"/>
-        <location filename="mainwindow.ui" line="180"/>
-        <location filename="mainwindow.ui" line="212"/>
-        <location filename="mainwindow.ui" line="244"/>
-        <location filename="mainwindow.ui" line="281"/>
-        <location filename="mainwindow.ui" line="313"/>
-        <location filename="mainwindow.ui" line="345"/>
+        <location filename="mainwindow.ui" line="107"/>
+        <location filename="mainwindow.ui" line="161"/>
+        <location filename="mainwindow.ui" line="373"/>
+        <location filename="mainwindow.ui" line="451"/>
+        <source>Margin:</source>
+        <translation>保证金：</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="127"/>
+        <location filename="mainwindow.ui" line="175"/>
+        <location filename="mainwindow.ui" line="207"/>
+        <location filename="mainwindow.ui" line="258"/>
+        <location filename="mainwindow.ui" line="290"/>
+        <location filename="mainwindow.ui" line="336"/>
+        <location filename="mainwindow.ui" line="387"/>
+        <location filename="mainwindow.ui" line="419"/>
+        <location filename="mainwindow.ui" line="465"/>
         <source>Fee:</source>
         <translation>手续费：</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="101"/>
-        <location filename="mainwindow.ui" line="170"/>
-        <location filename="mainwindow.ui" line="331"/>
+        <location filename="mainwindow.ui" line="151"/>
+        <location filename="mainwindow.ui" line="234"/>
+        <location filename="mainwindow.ui" line="437"/>
         <source>Selling price:</source>
         <translation>卖出价格：</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="129"/>
-        <location filename="mainwindow.ui" line="198"/>
-        <location filename="mainwindow.ui" line="299"/>
+        <location filename="mainwindow.ui" line="193"/>
+        <location filename="mainwindow.ui" line="276"/>
+        <location filename="mainwindow.ui" line="405"/>
         <source>Profilt:</source>
         <translation>赢利：</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="162"/>
+        <location filename="mainwindow.ui" line="226"/>
         <source>Shorts profit:</source>
         <translation>多头赢利：</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="263"/>
+        <location filename="mainwindow.ui" line="244"/>
+        <location filename="mainwindow.ui" line="322"/>
+        <source>Maring:</source>
+        <translation>保证金：</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="355"/>
         <source>Long profit:</source>
         <translation>空头赢利：</translation>
     </message>
